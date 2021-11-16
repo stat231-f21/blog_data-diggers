a747b704681bb71f1bc7437e79e1632c11f180ae
KarlaM315 <lmunoz24@amherst.edu>
2021-11-16 12:48:33 -0500

    Updated Shiny App w/ map

--- MapApp/app.R
+++ MapApp/app.R
@@ -1,49 +1,83 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
+## loading packages
+library(tidyverse)
 library(shiny)
+library(maps)
+library(plotly)
+library(leaflet)
+library(sf)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# import data
+kickstarter <- read.csv("map_tbl.csv")
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# remove the state "all"
+kickstarter <- kickstarter %>% 
+  filter(state != "All")
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+us_map <- maps::map("state", plot = FALSE, fill = TRUE) %>%
+  st_as_sf()
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+data(state)
+state_info <- data.frame(ID = tolower(state.name),
+                         state = state.name)
+
+# cuts it down from 51 states to 48
+new_map <- inner_join(kickstarter, state_info, by = "state") 
+new_map <- inner_join(new_map, us_map, by = "ID")
+m <- st_as_sf(new_map)
+
+############
+#    ui    #
+############
+
+cat_choices <- unique(m$main_category)
+
+ui <- navbarPage(
+  title = "Kickstarter",
+  
+  tabPanel(
+    
+    title = "Chloropleth Map",
+    
+    sidebarPanel(
+      
+      selectInput(
+        inputId = "widget",
+        label = "Choose Main Category",
+        multiple = FALSE,
+        choices = cat_choices,
+        selected = "All"
+      ),
+      
+      mainPanel(
+        plotOutput(outputId = "map") 
+        
+      ) 
     )
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  output$map <- renderPlot({
+    new_map <- m %>%
+      filter(main_category == input$widget) 
+    
+    ggplot(new_map) + geom_sf(aes(fill = success_rate)) + 
+      # make a plain theme
+      theme(axis.line = element_blank(),axis.text.x = element_blank(),
+            axis.text.y = element_blank(),axis.ticks = element_blank(),
+            axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            panel.background = element_blank(),panel.border = element_blank(),panel.grid.major=element_blank(),
+            panel.grid.minor = element_blank(),plot.background = element_blank()) 
+    
+  })
+  
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
+
+
+
+
